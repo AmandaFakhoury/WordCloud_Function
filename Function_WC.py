2f1df90d4832c0576ee24915fa752e9e993b7250
Amanda Fakhoury <47570353+AmandaFakhoury@users.noreply.github.com>
2019-09-09 11:37:36 -0400

    Add files via upload
    New Version

---
+++ Function_WC.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+import pandas as pd
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from os import path, getcwd
+import json
+
+def wordcloud_data(List , choice):
+    
+    #obj = json.loads(data) 
+    
+    #text = ' '.join(List)
+    
+    #df = pd.read_csv(str(csv))
+    
+    #choice = input("Data or wordcloud or both?")
+    
+    if choice == 'data':
+        
+        text = ' '.join(List)
+        
+        #df_1 = df.loc[df['Title'] == str(title)]
+        
+        
+        #text = " ".join(review for review in df_1.Clean_Text)
+        
+        #text = obj[str(title)]
+        
+        wordlist = text.split()
+        
+        wordfreq = [wordlist.count(p) for p in wordlist]
+        
+        dict = {'Words': wordlist, 'Frequency': wordfreq}
+        
+        Dict = dict
+        
+        freq_data = pd.DataFrame.from_dict(Dict)
+        
+        fd = freq_data.loc[freq_data['Frequency'] >= 3]
+        
+        fd = fd.drop_duplicates(keep='first')
+        
+        print(fd.head())
+
+        return fd.head()
+        
+    elif choice == 'wordcloud' :
+    
+        
+        import matplotlib.pyplot as plt
+        
+        text = ' '.join(List)
+        #df_1 = df.loc[df['Title'] == str(title)]
+        
+        #text = " ".join(review for review in df_1.Clean_Text)
+        
+        #text = obj[str(title)]
+        
+        WC = WordCloud(background_color="white" , max_font_size = 50,max_words = 200, height = 400, width = 700, collocations = False)
+        
+        dic = WC.process_text(text)
+
+        test = {}
+        for k, v in dic.items():
+            if v >= 3:
+                test.update({k:v})
+
+
+        img = WC.fit_words(test)  #buiding the wordcloud with the updated frequency dictinary
+
+        plt.imshow(img)
+        plt.axis('off')
+        plt.tight_layout(pad=0)
+        plt.show()
+    
+    else:
+        
+        import matplotlib.pyplot as plt
+        
+        text = ' '.join(List)
+        
+        #df_1 = df.loc[df['Title'] == str(title)]
+        
+        #text = " ".join(review for review in df_1.Clean_Text)
+        
+        #text = obj[str(title)]
+        
+        
+        wordlist = text.split()
+        
+        wordfreq = [wordlist.count(p) for p in wordlist]
+        
+        dict = {'Words': wordlist, 'Frequency': wordfreq}
+        
+        Dict = dict
+        
+        print(Dict)
+        
+        freq_data = pd.DataFrame.from_dict(Dict)
+        
+        fd = freq_data.loc[freq_data['Frequency'] >= 3]
+        
+        fd = fd.drop_duplicates(keep='first')
+        
+        print(fd.head())
+        
+        #text = " ".join(review for review in df_1.Clean_Text)
+        
+        WC = WordCloud(background_color="white" , max_font_size = 50,max_words = 200, height = 400, width = 700, collocations = False)
+        
+        dic = WC.process_text(text)
+
+        test = {}
+        for k, v in dic.items():
+            if v >= 3:
+                test.update({k:v})
+
+
+        img = WC.fit_words(test)  #buiding the wordcloud with the updated frequency dictinary
+
+        plt.imshow(img)
+        plt.axis('off')
+        plt.tight_layout(pad=0)
+        plt.show()
+        
+        
+        
+if __name__ == '__main__':
+    wordcloud_data(*sys.argv[1:])
+    
+    
+
